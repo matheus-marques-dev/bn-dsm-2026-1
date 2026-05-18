1d576755c1cc3e645c3910c12737012bab205e8b
Matheus Marques <matheusms.dev@gmail.com>
2026-05-18 02:20:08 +0000

    feat: finalizando os prints do CRUD e atualizando controllers

--- back-end/src/bin/server.js
+++ back-end/src/bin/server.js
@@ -12,7 +12,7 @@ import { createServer } from 'http'
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '8888')
+const port = normalizePort(process.env.PORT || '8890')
 app.set('port', port)
 
 /**
